263991bea0f21c7f4db132dee3f6a74d3a74bd27
wiebkem <wickoby@gmail.com>
2018-02-23 11:11:14 +0100

    set the country and year

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,12 +1,22 @@
+/* years from 2007 - 2017 */
+var yearStart = 2007;
+var yearEnd = 2017;
+
 $(document).ready(function() {
     setYearSelectOptions();
     setCountrySelectOptions();
+
+    fetchVotes(yearEnd);
+
+    $('#yearSelection').change(function() {
+        fetchVotes($(this).val());
+    });
+    $('#countrySelection').change(function() {
+        renderVotes($(this).val());
+    });
 });
 
 function setYearSelectOptions() {
-    /* years from 2007 - 2017 */
-    var yearStart = 2007;
-    var yearEnd = 2017;
     for(var i = yearEnd; i >= yearStart; i--) {
       $("#yearSelection").append($('<option>', {
          value: i,
