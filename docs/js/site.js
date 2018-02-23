7114629103578779163c697a70773286009e435a
wiebkem <wickoby@gmail.com>
2018-02-23 10:17:13 +0100

    changed the years to 2007-2017

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     setYearSelectOptions();
+    setCountrySelectOptions();
 });
 
 function setYearSelectOptions() {
-    /* years from 1956 - 2017 */
-    var yearStart = 1956;
+    /* years from 2007 - 2017 */
+    var yearStart = 2007;
     var yearEnd = 2017;
     for(var i = yearEnd; i >= yearStart; i--) {
       $("#yearSelection").append($('<option>', {
